0df2ae167bc7aacbc7e08dec52ed7f482e8299e9
Kedhar Sairam <120275780+kedishzone@users.noreply.github.com>
2023-02-23 12:27:34 +0530

    Add files via upload

---
+++ ac_variables/aa.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Hello, World!");
+        System.out.print("Hello, World!\n");
+        System.out.println("Hello, World!");
+
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        int a = 10;
+        int b = 5;
+        System.out.println("c= " + a*b);
+    }
+}
