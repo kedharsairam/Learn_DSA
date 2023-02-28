351cc04a3aabb719ce6a5448b460315da6bf67a6
kedharzero <kedhar713@gmail.com>
2023-02-28 20:18:32 +0530

    28th feb

---
+++ ah_ifelsequestions/aa.java
@@ -0,0 +1,22 @@
+package ah_ifelsequestions;
+
+import java.util.Scanner;
+
+public class aa {
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if(a>=b && a>=c){
+                System.out.println("A");
+            }
+            else if(b>=c){
+                System.out.println("B");
+            }
+            else{
+                System.out.println("C");
+            }
+        }
+    }
+}
---
+++ ah_ifelsequestions/ac.java
@@ -0,0 +1,13 @@
+package ah_ifelsequestions;
+
+import java.util.Scanner;
+
+public class ac {
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            int marks = sc.nextInt();
+            String student = ((marks) >= 33)? "Pass" : "Fail";
+            System.out.println(student);
+        }
+    }
+}
